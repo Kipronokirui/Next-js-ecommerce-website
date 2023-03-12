a65d14105748fd15518401671c20fc5eabf07f7d
unknown <victorkipronokirui2@gmail.com>
2023-03-12 20:35:47 +0300

    first commit

---
+++ components/Layout.js
@@ -0,0 +1,52 @@
+import React, { useContext, useEffect, useState } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import { Store } from "@/utils/Store";
+
+function Layout({title, children }) {
+  // Handling UI for add to cart 
+  const { state, dispatch } = useContext(Store);
+  const { cart } = state;
+  const [cartItemsCount, setCartItemsCount] = useState(0)
+  useEffect(() => {
+    setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
+  }, [cart.cartItems]);
+  
+  return (
+    <>
+      <Head>
+        <title>{title ? title + " - Amazona" : "Amazona"}</title>
+        <meta name="description" content="Ecommerce Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex min-h-screen flex-col justify-between ">
+        <header>
+          <nav className="flex h-12 items-center px-4 justify-between shadow-md">
+            <Link href="/" className="text-lg font-bold">
+              Amazona
+            </Link>
+            <div>
+              <Link href="/cart" className="p-2">
+                Cart
+                {cartItemsCount > 0 && (
+                  <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                    {cartItemsCount}
+                  </span>
+                )}
+              </Link>
+              <Link href="/dashboard" className="p-2">
+                Login
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          <p>Copyright © 2022 Amazona</p>
+        </footer>
+      </div>
+    </>
+  );
+}
+
+export default Layout;
